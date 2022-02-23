1059dc582322e24f6fbdd5e694a19ca557b7481a
bouhartsev <m3773517@gmail.com>
2022-02-24 00:42:24 +0300

    Add project files.

---
+++ AllTasks/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace AllTasks
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.lblName = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.task1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.task2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.task3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.task4ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.task5ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblCopyright = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip.SuspendLayout();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.statusStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.lblName,
+            this.lblCopyright});
+            this.statusStrip.Location = new System.Drawing.Point(0, 482);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.statusStrip.ShowItemToolTips = true;
+            this.statusStrip.Size = new System.Drawing.Size(790, 26);
+            this.statusStrip.SizingGrip = false;
+            this.statusStrip.TabIndex = 4;
+            this.statusStrip.Text = "Footer";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoToolTip = true;
+            this.lblName.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(134, 20);
+            this.lblName.Text = "Matvey Bouhartsev";
+            this.lblName.ToolTipText = "https://bouhartsev.top";
+            this.lblName.Click += new System.EventHandler(this.lblName_Click);
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.task1ToolStripMenuItem,
+            this.task2ToolStripMenuItem,
+            this.task3ToolStripMenuItem,
+            this.task4ToolStripMenuItem,
+            this.task5ToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(790, 28);
+            this.menuStrip.TabIndex = 4;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // task1ToolStripMenuItem
+            // 
+            this.task1ToolStripMenuItem.Name = "task1ToolStripMenuItem";
+            this.task1ToolStripMenuItem.Size = new System.Drawing.Size(62, 24);
+            this.task1ToolStripMenuItem.Text = "Task 1";
+            this.task1ToolStripMenuItem.Click += new System.EventHandler(this.task1ToolStripMenuItem_Click);
+            // 
+            // task2ToolStripMenuItem
+            // 
+            this.task2ToolStripMenuItem.Enabled = false;
+            this.task2ToolStripMenuItem.Name = "task2ToolStripMenuItem";
+            this.task2ToolStripMenuItem.Size = new System.Drawing.Size(62, 24);
+            this.task2ToolStripMenuItem.Text = "Task 2";
+            // 
+            // task3ToolStripMenuItem
+            // 
+            this.task3ToolStripMenuItem.Enabled = false;
+            this.task3ToolStripMenuItem.Name = "task3ToolStripMenuItem";
+            this.task3ToolStripMenuItem.Size = new System.Drawing.Size(62, 24);
+            this.task3ToolStripMenuItem.Text = "Task 3";
+            // 
+            // task4ToolStripMenuItem
+            // 
+            this.task4ToolStripMenuItem.Enabled = false;
+            this.task4ToolStripMenuItem.Name = "task4ToolStripMenuItem";
+            this.task4ToolStripMenuItem.Size = new System.Drawing.Size(62, 24);
+            this.task4ToolStripMenuItem.Text = "Task 4";
+            // 
+            // task5ToolStripMenuItem
+            // 
+            this.task5ToolStripMenuItem.Enabled = false;
+            this.task5ToolStripMenuItem.Name = "task5ToolStripMenuItem";
+            this.task5ToolStripMenuItem.Size = new System.Drawing.Size(62, 24);
+            this.task5ToolStripMenuItem.Text = "Task 5";
+            // 
+            // lblCopyright
+            // 
+            this.lblCopyright.BackColor = System.Drawing.Color.Transparent;
+            this.lblCopyright.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblCopyright.Name = "lblCopyright";
+            this.lblCopyright.Size = new System.Drawing.Size(58, 20);
+            this.lblCopyright.Text = "© 2022";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(154, 20);
+            this.toolStripStatusLabel1.Text = "Source Code (GItHub)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(790, 508);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.menuStrip);
+            this.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Bouhartsev";
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel lblName;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem task1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem task2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem task3ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem task4ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem task5ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripStatusLabel lblCopyright;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ AllTasks/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace AllTasks
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Form CreateTask<T>() where T : Form, new()
+        {
+            
+            Form task = new T();
+            task.MdiParent = this;
+            task.ControlBox = false;
+            task.WindowState = FormWindowState.Maximized;
+            task.FormBorderStyle = FormBorderStyle.None;
+            task.Show();
+            this.Width++; // костыль
+            this.Width--;
+            return task;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //
+        }
+
+        private void task1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form task = CreateTask<Task1.Form1>();
+            // task.Show();
+            // добавить закрытие и удаление предыдущего!!
+        }
+
+        private void lblName_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://bouhartsev.top");
+        }
+    }
+}
